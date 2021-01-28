8709aa6e109c5b04263805c895b18863fd5a4595
ShelterPush <38357508+ShelterPush@users.noreply.github.com>
2021-01-28 13:54:32 -0500

    Update food.py to give unique answers
    I added a random.shuffle() function and a foodlist.pop() function to give random answers while not repeating answers. Eventually the options run out. I don't know how to refill the options after they run out for a specific set of options.

--- food.py
+++ food.py
@@ -1,16 +1,14 @@
 # Script designed to aid in decided what to eat.
 import random
-#import numpy as np
 
-cheap = ["McDonalds", "Arbys", "Cookout", "Taco Bell", "Waffle House", "Subway", "Captain D's", "Bojangles", "Little Caesar's"]
-notcheap = ["O'Charleys", "Chilis", "Chick-fil-a", "Moe's", "Cracker Barrel", "Zaxby's", "Papa John's"]
+cheap = ["McDonalds", "Subway", "Little Caesar's"]
+notcheap = ["O'Charleys", "Chilis", "Moe's"]
 either = cheap + notcheap
-takeoutonly = ["McDonalds", "Arbys", "Cookout", "Taco Bell", "Subway", "Captain D's", "Bojangles", "Little Caesar's", "Chick-fil-a", "Moe's", "Cracker Barrel", "Zaxby's", "Papa John's"]
+takeoutonly = ["McDonalds", "Subway", "Little Caesar's"]
 
 goodenough = "null"
 noblock = ["no"]
-#xlast = [0]
-#while goodenough is not "no":
+
 while goodenough not in noblock:
         choices = ["in", "out"]
         takeoutoreatin = input("""Dine in or take out? Type 'in' or 'out'
@@ -35,13 +33,16 @@ while goodenough not in noblock:
                         foodlist = notcheap
                 if cheapornot == "either":
                         foodlist = either
-        length = len(foodlist) - 1
-        x = random.randint(1,length)
-        #while x in xlast:
-                #x = random.randint(1,length)
-        #xlast = np.append(xlast, x)
-        print("""
-You should try""",foodlist[x])
+        #length = len(foodlist) - 1 # Old way of doing it
+        #x = random.randint(1,length)
+        random.shuffle(foodlist)
+        if foodlist == []:
+                print("""
+You have seen all available restaurants for the options given.
+If you would like to see them again, please choose 'no' and reboot the script.""")
+        else:
+                print("""
+You should try""",foodlist.pop()) # used to be foodlist[x]
         choices3 = ["yes", "no"]
         goodenough = input("""Would you like another suggestion? Type 'yes' or 'no'
 """)
@@ -52,3 +53,4 @@ You should try""",foodlist[x])
 
 input("""
 Press the Enter key to close this window""")
+exit()
